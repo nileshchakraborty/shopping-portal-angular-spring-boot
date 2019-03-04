7828433ea434cec2f8d9cd29f3eae936f5aacb23
nilesh <nileshchakraborty@gmail.com>
2019-03-04 17:57:27 -0500

    Initial commit

---
+++ backend/shopping-portal-service/src/main/java/co/ksco/shoppingportalservice/ShoppingPortalServiceApplication.java
@@ -0,0 +1,13 @@
+package co.ksco.shoppingportalservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingPortalServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingPortalServiceApplication.class, args);
+	}
+
+}
---
+++ backend/shopping-portal-service/src/test/java/co/ksco/shoppingportalservice/ShoppingPortalServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package co.ksco.shoppingportalservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShoppingPortalServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
